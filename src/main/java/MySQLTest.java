6b48a24400cd90eeea8cd5c2dfbcfa37fb8eb0ec
brickstate <brickersh@appstate.edu>
2025-04-08 17:17:22 -0400

    I was able to get the UNO game server running and I implemented the correct connection setup inside of the running server. I also added in a frick ton of documentation into the UnoServer Class in order to direct us as we continue to fill the endpoints with functionality. I Also changed the pom file to recognize the UnoServer as the main class, you can just open a new terminal to test it with 'curl http://localhost:7000/testing'

--- src/main/java/MySQLTest.java
+++ src/main/java/MySQLTest.java
@@ -5,6 +5,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+// THIS CLASS IS BASICALLY JUST FOR TESTING NOW
+// in order to test the most basic functionality of the project 
+// 1. change the <mainClass> tag --> MySQLTEST
+// 2. follow the normal steps in the TESTING SHI document
+// you should see a very rudimenatry 
+// 1
+// testuser
+// testpass
+
+
+// I do plan on going back and changing up the user table schema to not require passwords to play
+
 public class MySQLTest {
     public static void main(String[] args) {
         String jdbcUrl = "jdbc:mysql://localhost:3306/GameDB";
--- src/main/java/UnoServer.java
+++ src/main/java/UnoServer.java
@@ -1,34 +1,124 @@
 package main.java;
 
 
-import io.javalin.Javalin;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+
+
+//This is the "Game Server" which in the grand scheme of the project should:
+//  1. listen to http endpoint conenctions for user interaction
+
+
+// How server works:
+// If you just open up the sql proxy on your local machine and then run the "MySQLTest" Class
+// The server will only do one querey and will time out after (?) ms
+// Running the server to make the connections will continually allow the java application to communicate 
+//   with the Cloud SQL Proxy in order to query further to the database
+
+//Things to fully test (in loose order) 
+// 1. Register a user [/register]
+// 2. Display All usernames [/users] (should already be fully functionaly just need to make sure)
+// 3. Disscuss and establish a standard way of getting the UnoServer to call the GameLOGIC functions
+//    - Game LOGIC should handle all turn validation/ JSON outputs for Discard Pile, Draw Pile, 
+//       Displaying Card Count in user hand
+//    !!! for the "I have one card and didnt say uno so now i must draw x more cards" mechanic
+//       == have some column in game state when they have claimed UNO or not (should make implementing the mechanic easier)
+// 4. GameState information (challenging) 
+//    - Starting the game [/startgame/newgame] || [startgame/validID/XXX]
+//    - List game id
+//      + from recently started games (where two playes are not INTO the play of the game)
+//    - Join a game id (to play)
+//      + check if that SPECIFIC game ID ended (players already played and you cant play a old game) 
+//    - Verbose HELP dialouge incase you forget the endpoints [/help]
+// 5. Other shi idk bruh 1->4 was all i could think about now
 
 public class UnoServer {
     public static void main(String[] args) {
+        //Necessary for the server to connect to Cloud SQL Proxy
+        String jdbcUrl = "jdbc:mysql://localhost:3306/GameDB";
+        //yes I hardcoded the user and password to the DB idc 
+        String user = "testuser";
+        String password = "123";
+    
         
-        
+        //Keeps server from timing out and listens to enpoints
         Javalin app = Javalin.create().start(7000);
         
-        // Endpoint for testing
-        app.post("/users/register", ctx -> {
-            // Here you can access the body as a JSON object if you need it
-            System.out.println(ctx.body()); // Print the request body
-            ctx.result("User registered successfully!"); // Respond to the client
-        });
-
-
+        // get server running and test enpoints
         app.get("/", ctx -> ctx.result("Uno Game API is running!"));
 
         app.get("/hello", ctx -> ctx.result("Hello, world!"));
-
-        //app.post("/users/register", UserController.registerUser);
-        app.get("/users", UserController.getUsers);
+        
+        
+        //TODO get the enpoints working
+        //testingtheconnection
+        app.get("/testing", testingconnection());
 
         app.exception(Exception.class, (e, ctx) -> {
             ctx.status(500);
             ctx.result("Server error: " + e.getMessage());
         });
+
+        // Endpoint for testing
+        //app.post("/users/register", ctx -> {
+            // Here you can access the body as a JSON object if you need it
+        //    System.out.println(ctx.body()); // Print the request body
+        //    ctx.result("User registered successfully!"); // Respond to the client
+        //});
+
+        
+        //app.post("/users/register", UserController.registerUser);
+        //app.get("/users", UserController.getUsers);
+
     }
+
+    // TEST this function by adding more user names into the database
+    //   and see if the user names will print out one after the other
+    public static Handler testingconnection(){
+
+        return ctx -> {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/GameDB";
+        String user = "testuser";
+        String password = "123";
+
+        //needed for proper lambda return type
+        StringBuilder result = new StringBuilder();
+
+        //we want to do this to test the connection is working
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, user, password)) {
+            System.out.println("Connected to database!");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+            //this being outside of the while loop should only print this line ONCE
+            // not every time a new user is gathered to display
+            result.append("Listing USERS from (UNO) GameDB \n");
+
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String username = rs.getString("username");
+                //String passwordField = rs.getString("password");
+
+                //need to build the string to return to the /testing endpoint
+                result.append("ID: ").append(id)
+                      .append(", Username: ").append(username)
+                      .append("\n");
+            }
+
+            ctx.result(result.toString());
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
+  }
 }
 
