ddd0f34dac4fcf1e51d5ac1c2587c54cf6fe7010
Kaeden Lewis <lewiski@appstate.edu>
2025-04-09 20:27:57 -0400

    Fixing value?

--- src/main/java/Game_Parts/Card.java
+++ src/main/java/Game_Parts/Card.java
@@ -6,7 +6,9 @@ import Game_Parts.Types.Value;
 public class Card 
 {
     public Color color;
+    //public Value value;
     public Value value;
+
 
     public Card(Color color, Value value)
     {
